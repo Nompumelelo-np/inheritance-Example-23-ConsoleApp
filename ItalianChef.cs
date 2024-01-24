9d3c4fb08874f8eeddbcdfa09d1ee8f9915797a2
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-24 10:09:30 +0200

    Add project files.

---
+++ ItalianChef.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_Example_23_ConsoleApp
+{
+    internal class ItalianChef:Chef
+    {
+        public void MakesPasta()
+        {
+            Console.WriteLine("The Chef makes Pasta");
+        }
+        public override void MakeSpecialDish()
+        {
+            Console.WriteLine("The Chef makes Special Dishes");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_Example_23_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chef chef = new Chef();
+            chef.MakeChicken();
+
+            ItalianChef chef1 = new ItalianChef();
+            chef1.MakeChicken();
+            chef1.MakesPasta();
+            chef1.MakeSpecialDish();
+
+
+            Console.ReadLine();
+        }
+    }
+}
